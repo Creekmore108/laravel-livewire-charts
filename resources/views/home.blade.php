1427afa623a46147da86745cb898fa0cea346a4a
Steve Creekmore <Creekmore108@gmail.com>
2022-11-15 00:52:08 -0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Larvel Livewire 9 Livewire Charts</title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwindmin.min.css" rel="stylesheet">
+    <livewire:styles />
+</head>
+<body class="bg-gray-200 p-8">
+    <livewire:livewire-charts />
+    <livewire:scripts />
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"></script>  
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>  
+</body>
+</html>
